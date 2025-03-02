0b9875a5ba5af8fc9c5f3dfada21a88e997de72e
Dinesh <105980109+dineshkatta2802@users.noreply.github.com>
2025-03-02 07:34:42 +0530

    Updated

---
+++ JS/OPERATORS/Arithmetic_Operator.js
@@ -0,0 +1,57 @@
+//---Arithmetic operator
+//1.Addition Operator
+let x = 10 + 20;
+console.log(x);
+//Number + String => Concatenation 
+let y = 5+ "Hello";
+console.log(y);
+
+
+//Subtarction Operator
+let s = 10 - 20;
+console.log(s);
+
+let u = "Hello" -1;
+console.log(u);//NaN
+
+//Multiplication Operator
+let m = 10 * 20;
+console.log(m);
+
+let l = Infinity * 0;
+console.log(l);//NaN
+
+let t = Infinity * Infinity;
+console.log(t);
+
+let i = "Hi" * 2;
+console.log(i);
+
+//Division Operator
+let d = 10 / 20;
+console.log(d);//Infinity
+
+let o = "Hello" / 2;
+console.log(o);
+
+
+//Modulus Operator
+let a = 20 % 20;
+console.log(a);
+
+let b = 1 % -1;
+console.log(b);
+
+//Exponentiation Operator
+let c = 10 ** 20;
+console.log(c);
+
+//Increment Operator
+let e = 20;
+console.log(e++);
+console.log(++e);
+
+//Decrement Operator
+let f = 20;
+console.log(f--);
+console.log(--f);
