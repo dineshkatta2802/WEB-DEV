92fe6c7ab192ae9f8b0a0005ac596f4929761592
Dinesh <105980109+dineshkatta2802@users.noreply.github.com>
2025-06-05 13:56:08 +0530

    Updated

--- EXTENSIONS/popup.js
+++ EXTENSIONS/popup.js
@@ -1,1 +1,17 @@
-console.log("This is a popup!");
+let toggle = document.querySelector('.toggle');
+
+
+let isDark = false;
+
+toggle.addEventListener('click', () => {
+    isDark = !isDark;
+
+    document.body.classList.toggle('dark-mode');
+
+    // Icon toggle: moon ↔ suna
+    if (isDark) {
+        toggle.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+    } else {
+        toggle.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+    }
+});
