d0aca74831d7590801d55ae53da15ffccff3e9d1
Dinesh Katta <kattadinesh2@gmail.com>
2025-06-22 16:51:16 +0530

    Updated

--- JS/BINARY DATA AND FILES/ArrayBuffer.js
+++ JS/BINARY DATA AND FILES/ArrayBuffer.js
@@ -1,4 +1,12 @@
-let para =  document.querySelector('.para');
+let buffer = new ArrayBuffer(16);
+let view = new Uint32Array(buffer);
 
+console.log("view length : " + view.length);
+console.log("view byte length : " + view.byteLength);
 
-    
+view[0] = 1234;
+view[2] = 9101112;
+
+for(let i=0 ; i<view.length ; i++){
+    console.log(`view[${i}] : ` + i);
+}
