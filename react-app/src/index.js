43675810d37b16b68a8f027cf4f2196a471bc52c
Dinesh Katta <kattadinesh2@gmail.com>
2025-07-14 09:00:40 +0530

    Updated

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -18,12 +18,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Component from './Component'
 import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Component />
   </React.StrictMode>
 );
