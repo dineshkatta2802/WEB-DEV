3bbcaf8925f11dd51224e7ee1f25d35b5e2d909d
Dinesh Katta <kattadinesh2@gmail.com>
2025-07-14 15:18:30 +0530

    Updated

--- react-app/src/Component.js
+++ react-app/src/Component.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+function MyButton() {
+    return(
+        <button>Click Here!</button>
+    );
+}
+
 export default function App() {
     return (
-        <div></div>
+        <div>
+            <h1>My First Component in React</h1>
+            <MyButton/>
+        </div>
     );
 }
