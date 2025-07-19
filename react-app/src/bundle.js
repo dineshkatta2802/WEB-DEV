083cb194bd57e3ea2dc56487b6d6c81b1dba799d
Dinesh Katta <kattadinesh2@gmail.com>
2025-07-19 21:22:45 +0530

    Updated

---
+++ react-app/src/bundle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Sample(){
+    return(
+        <div>
+            Hello this is dinesh
+        </div>
+    );
+}
+
+export default function App(){
+    return(
+        <div>
+            <Sample/>
+        </div>
+    );
+}
