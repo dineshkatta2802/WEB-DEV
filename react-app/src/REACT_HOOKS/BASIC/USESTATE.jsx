bb276206d7baf1559ede332b248465028343601b
Dinesh Katta <kattadinesh2@gmail.com>
2025-12-29 14:07:33 +0530

    Updated

--- react-app/src/REACT_HOOKS/BASIC/USESTATE.jsx
+++ react-app/src/REACT_HOOKS/BASIC/USESTATE.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
+const sayHi = () => {
+    return  "Hello hi";
+}
+
 export default function App(){
     const [name, setName] = useState('Dinesh');
+
+    // Youc can pass a function as the initial state , but it shall has no arguments and has to be pure 
+    const [geertings, setGreetings] = useState(() => sayHi()); 
 
     function handleChange(e){
         setName(e.target.value);
     }
+
     return(
         <div>
             <h1>My name : {name}</h1>
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -24,7 +24,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // ----------------------------MY IMPORTED FILES---------------------------
 // import Project from "./REACT_HOOKS/3.USEREDUCER/FAKE_IMAGE_EDITOR/FIE.jsx"
-import Project from "./REACT_HOOKS/5.MISCELLANEOUS __PROJECTS/TASK_MANAGER/Main"
+// import Project from "./REACT_HOOKS/5.MISCELLANEOUS __PROJECTS/TASK_MANAGER/Main"
+import Project from "./REACT_HOOKS/1.USESTATE/GESTURE_PATTERN_VAULT/GPV"
 // import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
