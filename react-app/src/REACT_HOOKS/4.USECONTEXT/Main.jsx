7c7cafda2b94283fb85dc5f2a80352574b045059
Dinesh Katta <kattadinesh2@gmail.com>
2026-01-27 18:44:37 +0530

    Updated

--- react-app/src/REACT_HOOKS/4.USECONTEXT/Main.jsx
+++ react-app/src/REACT_HOOKS/4.USECONTEXT/Main.jsx
@@ -7,17 +7,17 @@ export const ThemeContext = React.createContext(); // the context is created
 export default function Main(){
     const [darkTheme, setDarkTheme] =  useState(true);
 
-    const handleTheme = () => {
+    const handleTheme = () => { 
         setDarkTheme(prev => !prev);// flase / true
     }
 
     return(
-        <>
+        <>  
             {/* Passing down the props to the child components */}
             <ThemeContext.Provider value={darkTheme}>
                 <button onClick={handleTheme}>Dark Toggle</button>
                 <ClassContext/>
-                <FunctionContext/>
+                <FunctionContext/>''
             </ThemeContext.Provider>  
         </>
     );
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -25,7 +25,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 // ----------------------------MY IMPORTED FILES---------------------------
 // import Project from "./REACT_HOOKS/3.USEREDUCER/FAKE_IMAGE_EDITOR/FIE.jsx"
 // import Project from "./REACT_HOOKS/5.MISCELLANEOUS __PROJECTS/TASK_MANAGER/Main"
-import Project from "./REACT_HOOKS/1.USESTATE/GESTURE_PATTERN_VAULT/GPV"
+import Project from "./REACT_HOOKS/1.USESTATE/FONT-PLAYGROUND/FP"
 // import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
