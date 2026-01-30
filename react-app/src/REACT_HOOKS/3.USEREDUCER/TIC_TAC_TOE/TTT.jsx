75e8d7d5066b89c150e811961d7b371bc53bba11
Dinesh Katta <kattadinesh2@gmail.com>
2026-01-30 19:21:36 +0530

    Updated

--- react-app/src/REACT_HOOKS/3.USEREDUCER/TIC_TAC_TOE/TTT.jsx
+++ react-app/src/REACT_HOOKS/3.USEREDUCER/TIC_TAC_TOE/TTT.jsx
@@ -1,66 +1,10 @@
 import React, { useReducer } from 'react';
-import './TTT.css'
 
-const initialState = {
-    board : Array(9).fill(null),
-    xIsNext : true,
-    winner : null,
-    isDraw : false,
-};
-
-// Function to check winner
-const calcWinner = (square) => {
-    const lines = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6],[1,4,7],[2,5,8],
-        [0,4,8],[2,4,6]
-    ]
-
-    for(let [a,b,c] of lines){
-        if(square[a] && square[a] === square[b] && square[a] === square[c]){
-            return square[a];
-        }
-    }
-    return null;
-};
-
-const reducer = (state, action) =>{
-    switch(action.type){
-        case 'onCellClick': 
-            const index = action.index;
-            if(state.board[index] || state.winner || state.isDraw) return state;
-            const boardCopy = [...state.board];
-            boardCopy[index] = state.xIsNext ? '❌' : '⭕';
-            const winner = calcWinner(boardCopy);
-            const isDraw = !winner && boardCopy.every((sq) => sq!==null);
-            return {...state, board : boardCopy, xIsNext : !state.xIsNext, winner, isDraw};
-
-        case 'Reset':
-            return initialState;
-
-        default : 
-        return state;
-    }
-}
-
-export default function TTT(){
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const getStatus = () => {
-        if(state.winner) return `Winner ${state.winner}`;
-        if(state.isDraw) return 'Draw';
-        return `Next Player: ${state.xIsNext ? 'X' : 'O'}`;
-    }
-
+export default function App(){
+    const [state , dispatch] = useReducer(reducer, initialState);
     return(
-        <div className="Con">
-            <h1>{getStatus()}</h1>
-            <div className='cells'>
-                {state.board.map((val, i) => (
-                    <button key={i} onClick={() => dispatch({type : 'onCellClick', index : i})}>{val}</button>
-                    ))}
-                <button onClick={() => dispatch({type : 'Reset'})}>Reset</button>
-            </div>
+        <div>
+
         </div>
     );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -25,7 +25,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 // ----------------------------MY IMPORTED FILES---------------------------
 // import Project from "./REACT_HOOKS/3.USEREDUCER/FAKE_IMAGE_EDITOR/FIE.jsx"
 // import Project from "./REACT_HOOKS/5.MISCELLANEOUS __PROJECTS/TASK_MANAGER/Main"
-import Project from "./REACT_HOOKS/1.USESTATE/FONT-PLAYGROUND/FP"
+import Project from "./REACT_HOOKS/1.USESTATE/HOTEL_MANAGEMENT/MainHotel.jsx"
 // import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
