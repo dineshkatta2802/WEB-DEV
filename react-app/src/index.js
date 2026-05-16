b2b0e740206524c3848282a8ad35880eaefecdcb
Dinesh <kattadinesh2@gmail.com>
2026-05-17 04:55:42 +0530

    Updated

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -23,7 +23,7 @@ import ReactDOM from "react-dom/client";
 // import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // ----------------------------MY IMPORTED FILES---------------------------
-import Project from "./5. REACT_HOOKS/6.USEMEMO/Main.jsx";
+import Project from "./MISCELLANEOUS_PROJECTS/MINI/SPEED_TYPING_DASHBOARD/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
