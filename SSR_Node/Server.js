88d30b565d63c286c638232d5ab9607acbe90e5f
Dinesh Katta <kattadinesh2@gmail.com>
2026-08-27 18:32:28 +0530

    Updated

--- SSR_Node/Server.js
+++ SSR_Node/Server.js
@@ -1,34 +1,29 @@
-const http = require("node:http");
-const fs = require("fs");
-const path = require("path")
+const http = require('node:http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-    let filePath;
-
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    if(req.url === '/' && req.method === 'GET'){
-        filePath = path.join(__dirname,"Pages","Home.html")
+    if(req.url == '/' && req.method === 'GET'){
+        fs.readFile('./titles.json', (err, data) => {
+            if(err){
+                console.log(err);
+                res.end('Server Error');
+            }
+            else {
+                const titles = JSON.parse(data.toString());
+                fs.readFile('./index.html', (err,data) => {
+                    if(err){
+                        console.log(err);
+                        res.end('Server Error');
+                    }
+                    else {
+                        const index = data.toString();
+                        const html = index.replace('%', titles.join("<li></li>"))
+                        res.end(html);
+                    }
+                })
+            }
+        })
     }
-    else if(req.url === '/about' && req.method === 'GET'){
-        filePath = path.join(__dirname,"Pages","About.html")
-    }
-    else if(req.url === '/contact' && req.method === 'GET'){
-        filePath = path.join(__dirname,"Pages","Contact.html")
-    }
-    else {
-        res.statusCode = 404;
-        filePath = path.join(__dirname,"Pages","404.html")
-    }
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-            return res.end('500 Internal Error')
-        }
-        res.end(data)
-    })
-    
 }).listen(3000, () => {
     console.log("Server : http://localhost:3000");
 })
